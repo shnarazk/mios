7396cb10aa76aae32a23af20aae2024cb042169b
shnarazk <narazaki@nagasaki-u.ac.jp>
2016-04-05 09:51:34 +0900

    reset random decision rate to 0; update executable names
                           -r 0                             -r 20
        problem     #1      #2      #3      median  #1      #2     #3       median
        100         0.83    0.80    0.79    0.80    0.88    0.81    0.77    0.81
        125         1.89    1.42    1.19    1.42    1.98    1.77    2.04    1.98
        150         3.88    3.69    3.57    3.69    3.86    3.74    4.12    3.86
        175         9.19    10.08   9.83    9.83    11.64   10.95   12.14   11.64
        200         22.05   22.65   21.68   22.05   22.62   23.68   22.93   22.93
        225         41.93   42.84   43.30   42.84   55.63   56.52   56.44   56.44
        250         126.61  125.20  126.67  126.61  175.93  175.87  176.74  175.93
        een         7.05    7.00    7.07    7.05    7.49    7.45    7.45    7.45
        bmc         3.09    3.03    3.05    3.05    3.94    3.93    3.97    3.94
        38b         20.46   20.61   20.12   20.46   32.41   29.80   32.33   32.33

--- SAT/Solver/Mios/Internal.hs
+++ SAT/Solver/Mios/Internal.hs
@@ -27,4 +27,4 @@ data MiosConfiguration = MiosConfiguration
 -- Minisat-1.14 uses (0.95, 0.999, 0.2 = 20 / 1000).
 -- Minisat-2.20 uses (0.95, 0.999, 0).
 defaultConfiguration :: MiosConfiguration
-defaultConfiguration = MiosConfiguration 0.95 0.999 20 False
+defaultConfiguration = MiosConfiguration 0.95 0.999 0 False
