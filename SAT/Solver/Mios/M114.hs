bb4baabc3003cd4cb3b339cdaa5ffb83af3f8dcc
shnarazk <shuji.narazaki@gmail.com>
2016-08-11 15:48:56 +0900

    revert `44fbd4a`

--- SAT/Solver/Mios/M114.hs
+++ SAT/Solver/Mios/M114.hs
@@ -456,7 +456,6 @@ propagate s@Solver{..} = do
                           result <- enqueue s first c
                           if not result
                             then do
-                                setBool (protected c) True
                                 ((== 0) <$> decisionLevel s) >>= (`when` setBool ok False)
                                 setInt qHead =<< sizeOfStack trail
                                 -- Copy the remaining watches:
