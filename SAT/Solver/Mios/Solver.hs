c32d1b113d429f871b48dc2dc6342e1888416a9f
shnarazk <narazaki@nagasaki-u.ac.jp>
2016-08-23 09:29:21 +0900

    EXAM: paramerters = (6, ** 0.15)

--- SAT/Solver/Mios/Solver.hs
+++ SAT/Solver/Mios/Solver.hs
@@ -496,7 +496,7 @@ claRescaleActivityAfterRestart Solver{..} = do
       c <- getNthClause vec i
       d <- sizeOfClause c
       if d < 6
-        then modifyDouble (activity c) (** 0.2)
+        then modifyDouble (activity c) (** 0.15)
         else setDouble (activity c) 0
       setBool (protected c) False
       loopOnVector $ i + 1
