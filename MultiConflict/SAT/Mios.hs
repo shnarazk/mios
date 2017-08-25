f4dc5228b281fe21e34e546594f2001cd53bc565
shnarazk <narazakilaboratory@gmail.com>
2017-08-25 15:46:34 +0900

    MultiConflict/SAT/Mios.hs: update versionId

--- MultiConflict/SAT/Mios.hs
+++ MultiConflict/SAT/Mios.hs
@@ -48,7 +48,7 @@ import SAT.Mios.Validator
 
 -- | version name
 versionId :: String
-versionId = "Mios-1.5.0 -- https://github.com/shnarazk/mios"
+versionId = "Mios #MultiConflict based on 1.4.0 -- https://github.com/shnarazk/mios"
 
 reportElapsedTime :: Bool -> String -> Integer -> IO Integer
 reportElapsedTime False _ _ = return 0
