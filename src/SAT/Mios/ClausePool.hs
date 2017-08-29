e0e4a0cecfdd86368b7a67794de558f20eb6b1b5
shnarazk <narazakilaboratory@gmail.com>
2017-08-29 21:16:35 +0900

    src/SAT/Mios/ClausePool.hs: add functions

--- src/SAT/Mios/ClausePool.hs
+++ src/SAT/Mios/ClausePool.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE
     BangPatterns
+  , ViewPatterns
   #-}
 {-# LANGUAGE Trustworthy #-}
 
@@ -7,11 +8,14 @@ module SAT.Mios.ClausePool
        (
          ClausePool
        , newClausePool
-       , getPool
+       , makeClauseFromStack
+       , dumpToPool
        )
        where
 
 import qualified Data.Vector as V
+import SAT.Mios.Vec
+import SAT.Mios.Clause
 import qualified SAT.Mios.ClauseManager as CM
 
 -- | another immutable Vector of 'ClauseExtManager'
@@ -28,7 +32,29 @@ size2index n = if n < 16 then if n < 8  then 0
                                         else 3
 
 -- | returns 'CM.ClauseManager' for caluses which have suitable sizes.
-{-# INLINE getPool #-}
-getPool :: ClausePool -> Int -> CM.ClauseExtManager
-getPool p n = V.unsafeIndex p $ size2index n
+{-# INLINE getManager #-}
+getManager :: ClausePool -> Int -> CM.ClauseExtManager
+getManager p n = V.unsafeIndex p $ size2index n
 
+makeClauseFromStack :: ClausePool -> Stack -> IO Clause
+makeClauseFromStack pool v = do
+  n <- get' v
+  let mgr = getManager pool n
+  nn <- get' mgr
+  if nn == 0
+    then newClauseFromStack True v
+    else do c <- lastOf mgr
+            popFrom mgr
+            let lstack = lits c
+                loop ((<= n) -> False) = return ()
+                loop i = (setNth lstack i =<< getNth v i) >> loop (i + 1)
+            loop 0
+            set' (activity c) 0.0
+            set' (protected c) False
+            return c
+
+-- | Note: only learnt clauses are stocked.
+dumpToPool :: ClausePool -> Clause -> IO ()
+dumpToPool pool c = if learnt c
+                    then do n <- get' c; pushTo (getManager pool n) c
+                    else return ()
