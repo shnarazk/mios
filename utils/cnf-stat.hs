a0133daa2a0d38091d772c1121db21f1fa3fd0c7
shnarazk <narazakilaboratory@gmail.com>
2017-10-02 16:58:12 +0900

    utils/cnf-stat.hs: just count literals in a file

--- utils/cnf-stat.hs
+++ utils/cnf-stat.hs
@@ -6,7 +6,6 @@
 --   - the average number of literals in a clause /k/
 {-# LANGUAGE
     MultiWayIf
-  , ViewPatterns
   #-}
 module Main
        (
@@ -19,14 +18,11 @@ import qualified Data.ByteString.Char8 as B
 import Data.List
 import Numeric (showFFloat)
 import SAT.Mios
-import SAT.Mios.Types (getNth, get')
-import SAT.Mios.Clause (Clause(..))
-import SAT.Mios.ClauseManager (getClauseVector)
-import SAT.Mios.Solver (newSolver, Solver(..))
+import System.IO
 
 -- version id
 vid :: String
-vid = "cnf-stats version 0.1"
+vid = "cnf-stats version 0.2"
 
 usage :: String
 usage = miosUsage $ vid ++ "\n" ++ "Usage: cnf-stats cnf+"
@@ -37,21 +33,15 @@ main = do opts <- miosParseOptionsFromArgs vid
           if | _displayVersion opts -> putStrLn vid
              | _displayHelp opts    -> putStrLn usage
              | null (_targets opts) -> putStrLn usage
-             | otherwise            -> mapM_ (analyzeCNF opts) (_targets opts)
+             | otherwise            -> mapM_ (countLiterals opts) (_targets opts)
 
-analyzeCNF :: MiosProgramOption -> FilePath -> IO ()
-analyzeCNF opts cnfFile = do
+countLiterals :: MiosProgramOption -> FilePath -> IO ()
+countLiterals opts cnfFile = do
   (desc, cls) <- parseCNF (Just cnfFile) <$> B.readFile cnfFile
   let n = _numberOfVariables desc
+  let m = _numberOfClauses desc
   when (n /= 0) $ do
-    s <- newSolver (toMiosConf opts) desc
-    injectClausesFromCNF s desc cls
-    vec <- getClauseVector (clauses s)
-    m <- get' (clauses s)
-    let countLiterals :: Int -> Int -> IO Int
-        countLiterals ((< m) -> False) t = return t
-        countLiterals i t = countLiterals (i + 1) . (t +) =<< get' =<< getNth vec i
-    o <- countLiterals 0 0
+    let o = length . filter ("0" /=) . words $ B.unpack cls
     putStrLn $ intercalate ","
       [ cnfFile
       , show n
