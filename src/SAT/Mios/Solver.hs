43bebdff4d1cb8585f7e39a8931e8b4207b4333e
shnarazk <narazakilaboratory@gmail.com>
2017-11-18 07:50:54 +0900

    prepare 3; more 4e9836-ish

--- src/SAT/Mios/Solver.hs
+++ src/SAT/Mios/Solver.hs
@@ -320,8 +320,8 @@ clauseNew s@Solver{..} ps isLearnt = do
      -- Add clause to watcher lists:
      l1 <- getNth lstack 1
      l2 <- getNth lstack 2
-     pushClauseWithKey (getNthWatcher watches (negateLit l1)) c l2
-     pushClauseWithKey (getNthWatcher watches (negateLit l2)) c l1
+     pushClauseWithKey (getNthWatcher watches (negateLit l1)) c 0
+     pushClauseWithKey (getNthWatcher watches (negateLit l2)) c 0
      return (Right c)
 
 -- | __Fig. 9 (p.14)__
