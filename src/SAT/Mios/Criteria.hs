e3c3192301ab261e3ccdd754063ed44a4649eac5
shnarazk <narazakilaboratory@gmail.com>
2017-12-25 11:30:26 +0900

    stop log-scaling in blocking restart

--- src/SAT/Mios/Criteria.hs
+++ src/SAT/Mios/Criteria.hs
@@ -269,6 +269,11 @@ updateLBD s c@Clause{..} = do
 -- | #62
 checkRestartCondition :: Solver -> Int -> IO Bool
 checkRestartCondition s@Solver{..} (fromIntegral -> lbd) = do
+  -- k <- getStat s NumOfRestart
+  -- mode <- get' restartMode
+  next <- get' nextRestart
+  count <- getStat s NumOfBackjump
+  nas <- fromIntegral <$> nAssigns s
   let (co1, co2, co3, co4) = emaCoeffs config
       c1 = 2 ^ co1 :: Int
       c2 = 2 ^ co2 :: Int
@@ -278,19 +283,15 @@ checkRestartCondition s@Solver{..} (fromIntegral -> lbd) = do
       ema2 = 1 / fromIntegral c2 :: Double
       ema3 = 1 / fromIntegral c3 :: Double
       ema4 = 1 / fromIntegral c4 :: Double
-  k <- getStat s NumOfRestart
-  next <- get' nextRestart
-  count <- getStat s NumOfBackjump
-  nas <- fromIntegral <$> nAssigns s
-  let revise a f x  = do f' <- ((a * x) +) . ((1 - a) *) <$> get' f
+      -- gef = 1.1 :: Double       -- geometric expansion factor
+      revise :: Double -> Double' -> Double -> IO Double
+      revise a f x  = do f' <- ((a * x) +) . ((1 - a) *) <$> get' f
                          set' f f'
                          return f'
       rescale :: Int -> Double -> Double
-      rescale x y
-        | count == 0 = 0
-        | count < x  = fromIntegral x * y / fromIntegral count
-        | otherwise  = y
-      gef = 1.1 :: Double       -- geometric expansion factor
+      rescale x y = if | count == 0 -> 0
+                       | count < x  -> fromIntegral x * y / fromIntegral count
+                       | otherwise  -> y
       step = if | count < c1 -> 50
                 | count < c2 -> 95
                 | otherwise  -> 100
@@ -298,7 +299,6 @@ checkRestartCondition s@Solver{..} (fromIntegral -> lbd) = do
   ds <- rescale c2 <$> revise ema2 emaDSlow lbd
   af <- rescale c3 <$> revise ema3 emaAFast nas
   as <- rescale c4 <$> revise ema4 emaASlow nas
---  mode <- get' restartMode
   if | count < next   -> return False
 --     | mode == 1      -> do
 --         when (r2 < count && df < 2.0 * ds) $ set' restartMode 2 -- enter the second mode
@@ -310,7 +310,8 @@ checkRestartCondition s@Solver{..} (fromIntegral -> lbd) = do
 --         return True
      | 1.25 * as < af -> do
          incrementStat s NumOfBlockRestart 1
-         set' nextRestart (count + floor (fromIntegral step + gef ** fromIntegral k))
+         -- set' nextRestart (count + floor (fromIntegral step + gef ** fromIntegral k))
+         set' nextRestart (count + step)
          when (3 == dumpStat config) $ dumpSolver DumpCSV s
          return False
      | 1.25 * ds < df -> do
