4e9b51292758cb2df0204f3d024ad1bd4439c4a9
shnarazk <narazakilaboratory@gmail.com>
2018-04-03 17:22:06 +0900

    shorten versionId

--- src/SAT/Mios.hs
+++ src/SAT/Mios.hs
@@ -52,7 +52,7 @@ import SAT.Mios.Validator
 
 -- | version name
 versionId :: String
-versionId = "mios-81 -- https://github.com/shnarazk/mios"
+versionId = "#81 -- https://github.com/shnarazk/mios"
 
 reportElapsedTime :: Bool -> String -> Integer -> IO Integer
 reportElapsedTime False _ 0 = return 0
