4df5987785da4db11b3e03978f9b424c8bd2762c
shnarazk <narazakilaboratory@gmail.com>
2018-04-03 13:34:31 +0900

    +new clause parser

--- src/SAT/Mios.hs
+++ src/SAT/Mios.hs
@@ -241,6 +241,15 @@ dumpAssigmentAsCNF (Just fname) (UNSAT _) =
 dumpAssigmentAsCNF (Just fname) (SAT l) =
   withFile fname WriteMode $ \h -> do hPutStrLn h "s SAT"; hPutStrLn h . (++ " 0") . unwords $ map show l
 
+
+showPath :: FilePath -> String
+showPath str = replicate (len - length basename) ' ' ++ if elem '/' str then basename else basename'
+  where
+    len = 50
+    basename = reverse . takeWhile (/= '/') . reverse $ str
+    basename' = take len str
+
+{-
 --------------------------------------------------------------------------------
 -- DIMACS CNF Reader
 --------------------------------------------------------------------------------
@@ -326,10 +335,60 @@ readClause s buffer bvec stream = do
                                         setNth bvec l LiftedT
                                         loop (i + 1) b'
   loop 1 . skipComments . skipWhitespace $ stream
+-}
 
-showPath :: FilePath -> String
-showPath str = replicate (len - length basename) ' ' ++ if elem '/' str then basename else basename'
-  where
-    len = 50
-    basename = reverse . takeWhile (/= '/') . reverse $ str
-    basename' = take len str
+--------------------------------------------------------------------------------
+
+-- | parses the header of a CNF file
+parseCNF :: Maybe FilePath -> IO (CNFDescription, B.ByteString)
+parseCNF target@(Just cnfFile) = do
+  let -- format: p cnf n m, length "p cnf" == 5
+      parseP line = case parseInt (skipWhitespace (B.drop 5 line)) of
+                      (x, second) -> case B.readInt (skipWhitespace second) of
+                                       Just (y, _) -> CNFDescription x y target
+      seek :: B.ByteString -> IO (CNFDescription, B.ByteString)
+      seek !bs
+        | B.head bs == 'p' = return (parseP l, B.tail bs')
+        | otherwise = seek (B.tail bs')
+        where (l, bs') = B.span ('\n' /=) bs
+  seek =<< B.readFile cnfFile
+
+-- | parses ByteString then injects the clauses in it into a solver
+{-# INLINABLE injectClausesFromCNF #-}
+injectClausesFromCNF :: Solver -> CNFDescription -> B.ByteString -> IO ()
+injectClausesFromCNF s (CNFDescription nv nc _) bs = do
+  buffer <- newVec (int2lit (negate nv) + 1) 0 :: IO (Vec Int)
+  let skipComments :: B.ByteString -> B.ByteString
+      skipComments !s = case B.uncons s of -- __Pre-condition:__ we are on the benngining of a line
+                          Just ('c', b') -> skipComments . B.tail . B.dropWhile (/= '\n') $ b'
+                          _ -> s
+      readClause :: Int -> B.ByteString -> IO ()
+      readClause ((< nc) -> False) _ = return ()
+      readClause !i !stream = do
+        let loop :: Int -> B.ByteString -> IO ()
+            loop !j !b = case parseInt $ skipWhitespace b of
+                           (k, b') -> if k == 0
+                                      then do setNth buffer 0 $ j - 1
+                                              void $ addClause s buffer
+                                              readClause (i + 1) b'
+                                      else do setNth buffer j (int2lit k)
+                                              loop (j + 1) b'
+        loop 1 . skipComments . B.dropWhile (`elem` " \t\n") $ stream
+  readClause 0 bs
+
+{-# INLINE skipWhitespace #-}
+skipWhitespace :: B.ByteString -> B.ByteString
+skipWhitespace !s = B.dropWhile (== ' ') {- (`elem` " \t") -} s
+
+{-# INLINE parseInt #-}
+parseInt :: B.ByteString -> (Int, B.ByteString)
+parseInt !st = do
+  let !zero = ord '0'
+      loop :: (Int, B.ByteString) -> (Int, B.ByteString)
+      loop (val, s) = case B.uncons s of
+        Just (c, s') -> if '0' <= c && c <= '9' then loop (val * 10 + ord c - zero, s') else (val, s')
+        -- _ -> error (">>>>" ++ take 80 (B.unpack s))
+  case B.uncons st of
+    Just ('-', st') -> let (k, x) = loop (0, st') in (negate k, x)
+    Just ('0', st') -> (0, st')
+    _ -> loop (0, st)
