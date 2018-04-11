85053d4e4233ded4b723f7f7655bbe5a9dac3114
shnarazk <narazakilaboratory@gmail.com>
2018-04-11 16:42:26 +0900

    the second branch about proxy vector

--- src/SAT/Mios.hs
+++ src/SAT/Mios.hs
@@ -52,7 +52,7 @@ import SAT.Mios.Validator
 
 -- | version name
 versionId :: String
-versionId = "#72#73#74#77#78#80#79 https://github.com/shnarazk/mios"
+versionId = "#85.0 https://github.com/shnarazk/mios"
 
 reportElapsedTime :: Bool -> String -> Integer -> IO Integer
 reportElapsedTime False _ 0 = return 0
