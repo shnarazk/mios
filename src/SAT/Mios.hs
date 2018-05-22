47f1946c91c73b8e876f7ba2596614e9f429f656
shnarazk <narazakilaboratory@gmail.com>
2018-05-22 13:23:52 +0900

    package.yaml: drop '-A8m'

--- src/SAT/Mios.hs
+++ src/SAT/Mios.hs
@@ -52,7 +52,7 @@ import SAT.Mios.Validator
 
 -- | version name
 versionId :: String
-versionId = "#85.2 https://github.com/shnarazk/mios"
+versionId = "#85, #71 https://github.com/shnarazk/mios"
 
 reportElapsedTime :: Bool -> String -> Integer -> IO Integer
 reportElapsedTime False _ 0 = return 0
