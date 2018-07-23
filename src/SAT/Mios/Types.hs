b1d1e1bf97a741a5030735c778f5a11942448a54
shujinarazaki <shujinarazaki@protonmail.com>
2018-07-23 20:14:56 +0900

    use larger value for RestartExpansion

--- src/SAT/Mios/Types.hs
+++ src/SAT/Mios/Types.hs
@@ -332,7 +332,7 @@ data MiosConfiguration = MiosConfiguration
 -- * Mios-1.2     uses @(0.95, 0.999, 0)@.
 --
 defaultConfiguration :: MiosConfiguration
-defaultConfiguration = MiosConfiguration 0.95 0.999 0 (ef, es) 1.15 100
+defaultConfiguration = MiosConfiguration 0.95 0.999 0 (ef, es) 1.2 100
   where ef = (2 :: Int) ^ ( 5 :: Int)
         es = (2 :: Int) ^ (14 :: Int)
 
